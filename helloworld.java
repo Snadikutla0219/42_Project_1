6d6b1befdc542bae8d427fb3a85e6ccae3ea0dc5
Snadikutla0219 <Sandeep.nadikutla@gmail.com>
2026-01-22 09:23:14 +0530

    Change welcome message from GitHub to DevOps

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Sandeep welcomes you to GitHub");
+        System.out.println("Sandeep welcomes you to DevOps");
     }
 }
